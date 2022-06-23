b5df5f1b17613323cb0924173e651a917f46a9cb
Sier Santos <louisasantosl12@gmail.com>
2022-06-23 16:40:03 +0900

    Added initial code

--- Contact Tracing (updated)/Form1.Designer.cs
+++ Contact Tracing (updated)/Form1.Designer.cs
@@ -165,6 +165,8 @@
             this.CTlabelName1 = new System.Windows.Forms.Label();
             this.CTlabelAbout6 = new System.Windows.Forms.Label();
             this.CTlabelAbout5 = new System.Windows.Forms.Label();
+            this.CTbtnClear = new System.Windows.Forms.Button();
+            this.CTbtnSave = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -1558,11 +1560,32 @@
             this.CTlabelAbout5.TabIndex = 179;
             this.CTlabelAbout5.Text = "Contact Tracing List";
             // 
+            // CTbtnClear
+            // 
+            this.CTbtnClear.Location = new System.Drawing.Point(1183, 428);
+            this.CTbtnClear.Name = "CTbtnClear";
+            this.CTbtnClear.Size = new System.Drawing.Size(75, 25);
+            this.CTbtnClear.TabIndex = 242;
+            this.CTbtnClear.Text = "Clear";
+            this.CTbtnClear.UseVisualStyleBackColor = true;
+            // 
+            // CTbtnSave
+            // 
+            this.CTbtnSave.Location = new System.Drawing.Point(1264, 428);
+            this.CTbtnSave.Name = "CTbtnSave";
+            this.CTbtnSave.Size = new System.Drawing.Size(75, 25);
+            this.CTbtnSave.TabIndex = 241;
+            this.CTbtnSave.Text = "Save";
+            this.CTbtnSave.UseVisualStyleBackColor = true;
+            this.CTbtnSave.Click += new System.EventHandler(this.CTbtnSave_Click);
+            // 
             // ContactTracing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1362, 741);
+            this.Controls.Add(this.CTbtnClear);
+            this.Controls.Add(this.CTbtnSave);
             this.Controls.Add(this.CTlabelGuide4);
             this.Controls.Add(this.CTlabelGuide3);
             this.Controls.Add(this.CTlabelGuide2);
@@ -1845,6 +1868,8 @@
         private System.Windows.Forms.Label CTlabelName1;
         private System.Windows.Forms.Label CTlabelAbout6;
         private System.Windows.Forms.Label CTlabelAbout5;
+        private System.Windows.Forms.Button CTbtnClear;
+        private System.Windows.Forms.Button CTbtnSave;
     }
 }
 
--- Contact Tracing (updated)/Form1.cs
+++ Contact Tracing (updated)/Form1.cs
@@ -16,5 +16,24 @@ namespace Contact_Tracing__updated_
         {
             InitializeComponent();
         }
+
+        private void CTbtnSave_Click(object sender, EventArgs e)
+        {
+            string Name = TxtBoxFN.Text + " " + TxtBoxMN.Text + " " + TxtBoxSN.Text + " " + TxtBoxSufix.Text;
+            string Age = TxtBoxAge.Text;
+            string Birthday = CTcboxMonth.SelectedItem.ToString() + " " + CTcboxDay.SelectedItem.ToString() + " " + CTcboxYear.SelectedItem.ToString();
+            string SexFM = CTcboxSexFM.SelectedItem.ToString();
+            string PNumber = TxtBoxPN.Text;
+            string Email = TxtBoxEmail.Text;
+            string BirthPlace = TxtBoxBP.Text;
+            string CurrentAdd = TxtBoxCA.Text;
+            string MName = TxtBoxMotherFN.Text + " " + TxtBoxMotherMN.Text + " " + TxtBoxMotherSN.Text + " " + TxtBoxMotherSuffix.Text;
+            string MPNumber = TxtBoxMotherPN.Text;
+            string MCurrentAdd = TxtBoxMotherCA.Text;
+            string FName = TxtBoxFatherFN.Text + " " + TxtBoxFatherMN.Text + " " + TxtBoxFatherSN.Text + " " + TxtBoxFatherSuffix.Text;
+            string FPNumber = TxtBoxFatherPN.Text;
+            string FCurrentAdd = TxtBoxFatherCA.Text;
+
+        }
     }
 }
